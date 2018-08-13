dd6b8872ee8950f1ac1fa4404fe3b80fa8579ab7
Isaac Campos <kakoo26i@gmail.com>
2018-08-13 15:20:42 -0600

    Se agregan los iconos de los tooltips

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <gtk/gtk.h>
+#include <string.h>  
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,10 +8,15 @@ GtkBuilder      *builder;
 GtkWidget      *window, *about_window, *pending_window;
 //Declaración de botones
 GtkWidget       *rutas_cortas_btn, *tba1_btn, *tba2_btn, *tba3_btn, *tba4_btn;
+GtkWidget       *tt_tba0, *tt_tba1, *tt_tba2, *tt_tba3, *tt_tba4;
+
+//Declaración de funciones
+void myCSS(void);
 
 int main(int argc, char *argv[])
 {
     gtk_init(&argc, &argv);
+	myCSS();
 
     builder = gtk_builder_new();
     gtk_builder_add_from_file (builder, "glade/proyecto.glade", NULL);
@@ -23,12 +29,19 @@ int main(int argc, char *argv[])
     tba3_btn = GTK_WIDGET(gtk_builder_get_object(builder, "tba3"));
     tba4_btn = GTK_WIDGET(gtk_builder_get_object(builder, "tba4"));
 
+    //tooltips
+    tt_tba0 = GTK_WIDGET(gtk_builder_get_object(builder, "tt_tba0"));
+    tt_tba1 = GTK_WIDGET(gtk_builder_get_object(builder, "tt_tba1"));
+    tt_tba2 = GTK_WIDGET(gtk_builder_get_object(builder, "tt_tba2"));
+    tt_tba3 = GTK_WIDGET(gtk_builder_get_object(builder, "tt_tba3"));
+    tt_tba4 = GTK_WIDGET(gtk_builder_get_object(builder, "tt_tba4"));
+
     //Elementos de tooltip
-    gtk_widget_set_tooltip_text(rutas_cortas_btn, "Se utiliza el algoritmo X para encontrar las rutas más cortas");
-    gtk_widget_set_tooltip_text(tba1_btn, "Este algoritmo se encuentra en construcción");
-    gtk_widget_set_tooltip_text(tba2_btn, "Este algoritmo se encuentra en construcción");
-    gtk_widget_set_tooltip_text(tba3_btn, "Este algoritmo se encuentra en construcción");
-    gtk_widget_set_tooltip_text(tba4_btn, "Este algoritmo se encuentra en construcción");
+    gtk_widget_set_tooltip_text(tt_tba0, "Se utiliza el algoritmo X para encontrar las rutas más cortas");
+    gtk_widget_set_tooltip_text(tt_tba1, "Este algoritmo se encuentra en construcción");
+    gtk_widget_set_tooltip_text(tt_tba2, "Este algoritmo se encuentra en construcción");
+    gtk_widget_set_tooltip_text(tt_tba3, "Este algoritmo se encuentra en construcción");
+    gtk_widget_set_tooltip_text(tt_tba4, "Este algoritmo se encuentra en construcción");
 
     about_window = GTK_WIDGET(gtk_builder_get_object(builder, "about_window"));
     pending_window = GTK_WIDGET(gtk_builder_get_object(builder, "pending_window"));
@@ -40,6 +53,23 @@ int main(int argc, char *argv[])
     gtk_main();
 
     return 0;
+}
+
+void myCSS(void){
+    GtkCssProvider *provider;
+    GdkDisplay *display;
+    GdkScreen *screen;
+
+    provider = gtk_css_provider_new ();
+    display = gdk_display_get_default ();
+    screen = gdk_display_get_default_screen (display);
+    gtk_style_context_add_provider_for_screen (screen, GTK_STYLE_PROVIDER (provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+
+    const gchar *style_file = "src/style.css";
+    GError *error = 0;
+
+    gtk_css_provider_load_from_file(provider, g_file_new_for_path(style_file), &error);
+    g_object_unref (provider);
 }
 
 // called when about menu button is clicked
