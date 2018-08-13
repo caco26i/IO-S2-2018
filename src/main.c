437814a7c30215f203ee97d3b3b025ff7c70ebeb
aabarcab <andreba82@hotmail.com>
2018-08-13 02:53:45 -0600

    Modificaciones a interfaz

--- src/main.c
+++ src/main.c
@@ -1,28 +1,44 @@
 #include <gtk/gtk.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-GtkBuilder      *builder; 
-GtkWidget       *window;
-GtkWidget 		*about_window;
-GtkWidget 		*pending_window; 
- 
+GtkBuilder      *builder;
+//Declaración de ventanas
+GtkWidget      *window, *about_window, *pending_window;
+//Declaración de botones
+GtkWidget       *rutas_cortas_btn, *tba1_btn, *tba2_btn, *tba3_btn, *tba4_btn;
+
 int main(int argc, char *argv[])
 {
- 
     gtk_init(&argc, &argv);
- 
+
     builder = gtk_builder_new();
     gtk_builder_add_from_file (builder, "glade/proyecto.glade", NULL);
- 
+
+    //Inicialización de widgets
     window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
+    rutas_cortas_btn = GTK_WIDGET(gtk_builder_get_object(builder, "rutas_cortas"));
+    tba1_btn = GTK_WIDGET(gtk_builder_get_object(builder, "tba1"));
+    tba2_btn = GTK_WIDGET(gtk_builder_get_object(builder, "tba2"));
+    tba3_btn = GTK_WIDGET(gtk_builder_get_object(builder, "tba3"));
+    tba4_btn = GTK_WIDGET(gtk_builder_get_object(builder, "tba4"));
+
+    //Elementos de tooltip
+    gtk_widget_set_tooltip_text(rutas_cortas_btn, "Se utiliza el algoritmo X para encontrar las rutas más cortas");
+    gtk_widget_set_tooltip_text(tba1_btn, "Este algoritmo se encuentra en construcción");
+    gtk_widget_set_tooltip_text(tba2_btn, "Este algoritmo se encuentra en construcción");
+    gtk_widget_set_tooltip_text(tba3_btn, "Este algoritmo se encuentra en construcción");
+    gtk_widget_set_tooltip_text(tba4_btn, "Este algoritmo se encuentra en construcción");
+
     about_window = GTK_WIDGET(gtk_builder_get_object(builder, "about_window"));
     pending_window = GTK_WIDGET(gtk_builder_get_object(builder, "pending_window"));
     gtk_builder_connect_signals(builder, NULL);
-     
+
     g_object_unref(builder);
- 
-    gtk_widget_show(window);                
+
+    gtk_widget_show(window);
     gtk_main();
- 
+
     return 0;
 }
 
@@ -39,7 +55,7 @@ void on_press_pending_window()
 	//about_window = create_about_window();
 	gtk_widget_show (pending_window);
 }
- 
+
 // called when window is closed
 void on_window_main_destroy()
 {
