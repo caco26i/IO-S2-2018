8414ddd9d8b799a8caac341f4a5c9f8eb2020db0
Isaac Campos <kakoo26i@gmail.com>
2018-08-28 01:56:58 -0600

    Se arregla el boton de  cerrar el pending

--- src/pending.c
+++ src/pending.c
@@ -1,19 +1,21 @@
 #include <gtk/gtk.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-GtkBuilder      *builder;
-//Declaración de ventanas
-GtkWidget      *window;
-
 
 int main(int argc, char *argv[])
 {
+    GtkBuilder      *builder;
+    GtkWidget       *window;
+
     gtk_init(&argc, &argv);
 
     builder = gtk_builder_new();
     gtk_builder_add_from_file (builder, "glade/pending.glade", NULL);
+
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
+    gtk_builder_connect_signals(builder, NULL);
+
+    g_object_unref(builder);
+
+    gtk_widget_show(window);
     gtk_main();
 
     return 0;
