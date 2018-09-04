0f9c1c00c8d7e00787096337747a9cb7d5a70f5b
Isaac Campos <kakoo26i@gmail.com>
2018-09-03 19:48:44 -0600

    Se agregan los 3 algoritmos de knapsack

--- src/knapsack.c
+++ src/knapsack.c
@@ -1,18 +1,38 @@
 #include <gtk/gtk.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct {
     char *name;
-    double value;
-    double weight;
-    double volume;
+    int weight;
+    int value;
+    int count;
 } item_t;
 
 item_t items[] = {
-        {"panacea", 3000.0, 0.3, 0.025},
-        {"ichor",   1800.0, 0.2, 0.015},
-        {"gold",    2500.0, 2.0, 0.002},
+        {"map",                      9,   150,   1},
+        {"compass",                 13,    35,   1},
+        {"water",                  153,   200,   2},
+        {"sandwich",                50,    160,   2},
+        {"glucose",                 15,    60,   2},
+        {"tin",                     68,    45,   3},
+        {"banana",                  27,    60,   3},
+        {"apple",                   39,    40,   3},
+        {"cheese",                  23,    30,   1},
+        {"beer",                    52,    10,   3},
+        {"suntan cream",            11,    70,   1},
+        {"camera",                  32,    30,   1},
+        {"T-shirt",                 24,    15,   2},
+        {"trousers",                48,    10,   2},
+        {"umbrella",                73,    40,   1},
+        {"waterproof trousers",     42,    70,   1},
+        {"waterproof overclothes",  43,    75,   1},
+        {"note-case",               22,    80,   1},
+        {"sunglasses",               7,    20,   1},
+        {"towel",                   18,    12,   2},
+        {"socks",                    4,    50,   1},
+        {"book",                    30,    10,   2},
 };
 
 int n = sizeof (items) / sizeof (item_t);
@@ -20,7 +40,125 @@ int *count;
 int *best;
 double best_value;
 
-void knapsack (int i, double value, double weight, double volume) {
+// solution for 0-1 Knapsack problem
+
+// A utility function that returns maximum of two integers
+int max(int a, int b) { return (a > b) ? a : b; }
+
+// Prints the items which are put in a knapsack of capacity W
+//void knapsack1_0(int W, int wt[], int val[], int n)
+//{
+//    int i, w;
+//    int K[n + 1][W + 1];
+//
+//    // Build table K[][] in bottom up manner
+//    for (i = 0; i <= n; i++) {
+//        for (w = 0; w <= W; w++) {
+//            if (i == 0 || w == 0)
+//                K[i][w] = 0;
+//            else if (wt[i - 1] <= w)
+//                K[i][w] = max(val[i - 1] +
+//                              K[i - 1][w - wt[i - 1]], K[i - 1][w]);
+//            else
+//                K[i][w] = K[i - 1][w];
+//        }
+//    }
+//
+//    // stores the result of Knapsack
+//    int res = K[n][W];
+//    printf("%d\n", res);
+//
+//    w = W;
+//    for (i = n; i > 0 && res > 0; i--) {
+//
+//        // either the result comes from the top
+//        // (K[i-1][w]) or from (val[i-1] + K[i-1]
+//        // [w-wt[i-1]]) as in Knapsack table. If
+//        // it comes from the latter one/ it means
+//        // the item is included.
+//        if (res == K[i - 1][w])
+//            continue;
+//        else {
+//
+//            // This item is included.
+//            printf("%d ", wt[i - 1]);
+//
+//            // Since this weight is included its
+//            // value is deducted
+//            res = res - val[i - 1];
+//            w = w - wt[i - 1];
+//        }
+//    }
+//}
+
+int *knapsack0_1 (int w) {
+    int i, j, a, b, *mm, **m, *s;
+    mm = calloc((n + 1) * (w + 1), sizeof (int));
+    m = malloc((n + 1) * sizeof (int *));
+    m[0] = mm;
+    for (i = 1; i <= n; i++) {
+        m[i] = &mm[i * (w + 1)];
+        for (j = 0; j <= w; j++) {
+            if (items[i - 1].weight > j) {
+                m[i][j] = m[i - 1][j];
+            }
+            else {
+                a = m[i - 1][j];
+                b = m[i - 1][j - items[i - 1].weight] + items[i - 1].value;
+                m[i][j] = a > b ? a : b;
+            }
+        }
+    }
+    s = calloc(n, sizeof (int));
+    for (i = n, j = w; i > 0; i--) {
+        if (m[i][j] > m[i - 1][j]) {
+            s[i - 1] = 1;
+            j -= items[i - 1].weight;
+        }
+    }
+    free(mm);
+    free(m);
+    return s;
+}
+
+int *boundedKnapsack (int w) {
+    int i, j, k, v, *mm, **m, *s;
+    mm = calloc((n + 1) * (w + 1), sizeof (int));
+    m = malloc((n + 1) * sizeof (int *));
+    m[0] = mm;
+    for (i = 1; i <= n; i++) {
+        m[i] = &mm[i * (w + 1)];
+        for (j = 0; j <= w; j++) {
+            m[i][j] = m[i - 1][j];
+            for (k = 1; k <= items[i - 1].count; k++) {
+                if (k * items[i - 1].weight > j) {
+                    break;
+                }
+                v = m[i - 1][j - k * items[i - 1].weight] + k * items[i - 1].value;
+                if (v > m[i][j]) {
+                    m[i][j] = v;
+                }
+            }
+        }
+    }
+    s = calloc(n, sizeof (int));
+    for (i = n, j = w; i > 0; i--) {
+        int v = m[i][j];
+        for (k = 0; v != m[i - 1][j] + k * items[i - 1].value; k++) {
+            s[i - 1]++;
+            j -= items[i - 1].weight;
+        }
+    }
+    free(mm);
+    free(m);
+    return s;
+}
+
+// Returns the maximum value with knapsack of
+// W capacity
+
+
+void unboundedKnapsackAux (int i, double value, double w) {
     int j, m1, m2, m;
     if (i == n) {
         if (value > best_value) {
@@ -31,35 +169,74 @@ void knapsack (int i, double value, double weight, double volume) {
         }
         return;
     }
-    m1 = weight / items[i].weight;
-    m2 = volume / items[i].volume;
-    m = m1 < m2 ? m1 : m2;
+    m = w / items[i].weight;
     for (count[i] = m; count[i] >= 0; count[i]--) {
-        knapsack(
-                i + 1,
+        unboundedKnapsackAux(
+                i+1,
                 value + count[i] * items[i].value,
-                weight - count[i] * items[i].weight,
-                volume - count[i] * items[i].volume
+                w - count[i] * items[i].weight
         );
     }
 }
+void unboundedKnapsack(int w) {
+        unboundedKnapsackAux (0, 0, w);
+};
 
 int main(int argc, char *argv[])
 {
     GtkBuilder      *builder;
     GtkWidget       *window;
 
+    int w = 300;
+
+
+    //< KNAPSACK 1/0
+    printf("\n\nKNAPSACK 1/0\n");
+    int i, n, tw = 0, tv = 0, *s;
+    n = sizeof (items) / sizeof (item_t);
+    s = knapsack0_1(w);
+    for (i = 0; i < n; i++) {
+        if (s[i]) {
+            printf("%-22s %5d %5d\n", items[i].name, items[i].weight, items[i].value);
+            tw += items[i].weight;
+            tv += items[i].value;
+        }
+    }
+    printf("%-22s %5d %5d\n", "weight, value:", tw, tv);
+
+    //</ KNAPSACK 1/0
+
+    //< KNAPSACK bounded
+    printf("\n\nKNAPSACK bounded\n");
+
+    int tc = 0;
+    s = boundedKnapsack(w);
+    for (i = 0; i < n; i++) {
+        if (s[i]) {
+            printf("%-22s %5d %5d %5d\n", items[i].name, s[i], s[i] * items[i].weight, s[i] * items[i].value);
+            tc += s[i];
+            tw += s[i] * items[i].weight;
+            tv += s[i] * items[i].value;
+        }
+    }
+    printf("%-22s %5d %5d %5d\n", "count, weight, value:", tc, tw, tv);
+    //</ KNAPSACK bounded
+
+    //< KNAPSACK unbounded
+    printf("\n\nKNAPSACK unbounded\n");
 
     count = malloc(n * sizeof (int));
     best = malloc(n * sizeof (int));
     best_value = 0;
-    knapsack(0, 0.0, 25.0, 0.25);
-    int i;
+    unboundedKnapsack(w);
+
     for (i = 0; i < n; i++) {
         printf("%d %s\n", best[i], items[i].name);
     }
     printf("best value: %.0f\n", best_value);
     free(count); free(best);
+    //</ KNAPSACK unbounded
+
 
 
     gtk_init(&argc, &argv);
