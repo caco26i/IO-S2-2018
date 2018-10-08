3609c7e8c4a1ba76924acd659bdb3c561bcfa50d
Isaac Campos <kakoo26i@gmail.com>
2018-10-07 22:28:04 -0600

    Se agrega la funcionalidad de cargar la tabla para ingresar los valores de reventa

--- src/equipos.c
+++ src/equipos.c
@@ -8,8 +8,75 @@ GtkBuilder *builder;
 
 //Declaración de widgets
 GtkWidget *window, *save, *spinButtonTimeLimit, *spinButtonLife, *spinButtonInitialCost, *scrolleInitialData;
+GtkWidget       *windowInitialReplace;
+GtkWidget       *windowTableData;
+GtkWidget       *windowFinalTable;
+
+GtkWidget       ***tableData;
+GtkWidget       *g_tableData;
+
+GtkWidget       *g_frame_manualEntry;
+GtkWidget       *g_frame_fileEntry;
+GtkWidget       *g_filechooser_btn;
+GtkWidget       *g_entry_fileName;
+GtkWidget       *g_spin_usefulLife;
+GtkWidget       *g_spinbutton_timeLimit;
+GtkWidget       *g_spinbutton_precioEquipo;
+GtkWidget       *windowSave;
+
+GtkWidget       *g_scrolledwindow_initialTableData;
+GtkWidget       *g_scrolledwindow_finalTable;
+GtkWidget       *g_scrolledwindow_optimalSolution;
+FILE            *file_tableData;
+
+const char *rowHeaderInitialTable[3] = {"Año","Precio de Reventa","Mantenimiento"};
+const char *rowHeaderFinalTable[4] = {"t","G(t)","Próximo","Ganancia"};
+
+int usefulLife = 0;
+int totalUsefulLife = 0;
+int initialCost = 0;
+int timeLimit = 0;
+
+char buffer[9];
+char row[9];
+
+int contado_x;
+
+int Amount;
+int timeLimitEquipment;
+
+FILE * fileTableData;
+
+typedef struct  {
+    int year[31];
+    int value;
+    int position;
+    int profit;
+} FinalTable;
 
 
+typedef struct {
+    int value;
+    int year;
+} candidate;
+
+typedef struct  {
+    int year;
+    int sale;
+    int maintenance;
+} InitialTable;
+
+typedef struct
+{
+    int year[30];
+    int position;
+} plans;
+
+void setTotalObjectsCount(int pTotalUsefulLife,int pInitialCost, int pTimeLimit) {
+    totalUsefulLife = pTotalUsefulLife;
+    initialCost = pInitialCost;
+    timeLimit = pTimeLimit;
+}
 
 void generar_archivo() {
     int i, j;
@@ -89,6 +156,61 @@ void leer_archivo() {
     //g_free (filename);
 }
 
+void createTableData() {
+    tableData = calloc(usefulLife,sizeof(GtkWidget**));
+
+    g_tableData = gtk_grid_new ();
+    gtk_container_add (GTK_CONTAINER (g_scrolledwindow_initialTableData), g_tableData);
+
+    char rowNumber[4];
+
+    for(int j = 0; j < usefulLife; j++) {
+        tableData[j] = calloc(3,sizeof(GtkWidget*));
+    }
+
+    for(int row =0; row < usefulLife; row++)
+    {
+        for(int column=0; column < 3; column++)
+        {
+            tableData[row][column] = gtk_entry_new();
+            gtk_grid_attach (GTK_GRID (g_tableData),tableData[row][column] , column, row, 1, 1);
+            if (row == 0){
+                gtk_entry_set_text (GTK_ENTRY(tableData[row][column]),rowHeaderInitialTable[column]);
+                gtk_widget_set_name(tableData[row][column],"header");
+                gtk_widget_set_sensitive(tableData[row][column],FALSE);
+            }
+            if (column ==0 && row!=0){
+                sprintf(rowNumber, "%d", row);
+                gtk_entry_set_text (GTK_ENTRY(tableData[row][column]),rowNumber);
+                gtk_widget_set_name(tableData[row][column],"header");
+                gtk_widget_set_sensitive(tableData[row][column],FALSE);
+            }
+            if (column == 0){
+                gtk_entry_set_width_chars(GTK_ENTRY(tableData[row][column]), 3);
+            } else if (column == 1) {
+                gtk_entry_set_width_chars(GTK_ENTRY(tableData[row][column]), 14);
+            }else {
+                gtk_entry_set_width_chars(GTK_ENTRY(tableData[row][column]), 12);
+            }
+        }
+    }
+    gtk_widget_show_all(g_scrolledwindow_initialTableData);
+}
+
+
+void on_cambiar_clicked() {
+    usefulLife = gtk_spin_button_get_value_as_int (GTK_SPIN_BUTTON(spinButtonLife)) + 1;
+    createTableData();
+}
+
+//void on_btn_getFile_clicked() {
+//    usefulLife = countUsefulLifeFiles (gtk_file_chooser_get_filename (GTK_FILE_CHOOSER(g_filechooser_btn)));
+//    int matrixData[usefulLife-1][2];
+//    startFill(matrixData,gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(g_filechooser_btn)));
+//    createTableDataFile(matrixData);
+//}
+
+
 void myCSS(void) {
     GtkCssProvider *provider;
     GdkDisplay *display;
@@ -120,6 +242,9 @@ int main(int argc, char *argv[]) {
   spinButtonLife = GTK_WIDGET(gtk_builder_get_object(builder, "spinButtonLife"));
   spinButtonInitialCost = GTK_WIDGET(gtk_builder_get_object(builder, "spinButtonInitialCost"));
   scrolleInitialData = GTK_WIDGET(gtk_builder_get_object(builder, "scrolleInitialData"));
+
+
+  g_scrolledwindow_initialTableData = GTK_WIDGET(gtk_builder_get_object(builder, "scrolleInitialData"));
 
   //Inicialización de widgets
   window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
