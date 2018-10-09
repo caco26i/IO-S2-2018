5d2961eefe85043a03869b69808608779214094b
aabarcab <andreba82@hotmail.com>
2018-10-09 01:31:57 -0600

    guardar archivo

--- src/equipos.c
+++ src/equipos.c
@@ -84,42 +84,6 @@ void setTotalObjectsCount(int pTotalUsefulLife, int pInitialCost, int pTimeLimit
     timeLimit = pTimeLimit;
 }
 
-void generar_archivo() {
-    int i, j;
-
-    GtkWidget *dialog;
-    char *filename = NULL;
-    FILE *fichero;
-    dialog = gtk_file_chooser_dialog_new("Guardar archivo", GTK_WINDOW(window),
-                                         GTK_FILE_CHOOSER_ACTION_SAVE,
-                                         ("Cancelar"), GTK_RESPONSE_CANCEL,
-                                         ("Guardar"), GTK_RESPONSE_ACCEPT, NULL);
-    gtk_file_chooser_set_do_overwrite_confirmation(GTK_FILE_CHOOSER(dialog), TRUE);
-    gint answer = gtk_dialog_run(GTK_DIALOG(dialog));
-    if (answer == GTK_RESPONSE_ACCEPT) {
-        filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
-        fichero = fopen(filename, "w+");
-
-        int inputAmount = gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(spinButtonInitialCost));
-        int inputTimeLimit = gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(spinButtonTimeLimit));
-
-
-        for (int row = 0; row < usefulLife; row++) {
-            for (int column = 0; column < 3; column++) {
-                fprintf(fichero, "%s;", (gtk_entry_get_text(GTK_ENTRY(tableData[row][column]))));
-            }
-            fprintf(fichero, "\n");
-
-        }
-        fprintf(fichero, "%d\n", inputAmount);
-        fprintf(fichero, "%d\n", inputTimeLimit);
-
-        fclose(fichero);
-        g_free(filename);
-    }
-    gtk_widget_destroy(dialog);
-    gtk_widget_set_sensitive(save, FALSE);
-}
 
 void leer_archivo() {
     int i, j = 0, k, fileSize = 0, entrySize;
@@ -224,6 +188,44 @@ void createTableData() {
         }
     }
     gtk_widget_show_all(g_scrolledwindow_initialTableData);
+}
+
+void generar_archivo() {
+    int i, j;
+
+    GtkWidget *dialog;
+    char *filename = NULL;
+    FILE *fichero;
+    dialog = gtk_file_chooser_dialog_new("Guardar archivo", GTK_WINDOW(window),
+                                         GTK_FILE_CHOOSER_ACTION_SAVE,
+                                         ("Cancelar"), GTK_RESPONSE_CANCEL,
+                                         ("Guardar"), GTK_RESPONSE_ACCEPT, NULL);
+    gtk_file_chooser_set_do_overwrite_confirmation(GTK_FILE_CHOOSER(dialog), TRUE);
+    gint answer = gtk_dialog_run(GTK_DIALOG(dialog));
+    if (answer == GTK_RESPONSE_ACCEPT) {
+        filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
+        fichero = fopen(filename, "w+");
+
+        int inputAmount = gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(spinButtonInitialCost));
+        int inputTimeLimit = gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(spinButtonTimeLimit));
+
+        fprintf(fichero, "%d\n", inputAmount);
+        fprintf(fichero, "%d\n", inputTimeLimit);
+
+        for (int row = 1; row < usefulLife; row++) {
+            for (int column = 0; column < 3; column++) {
+                fprintf(fichero, "%s;", (gtk_entry_get_text(GTK_ENTRY(tableData[row][column]))));
+            }
+            fprintf(fichero, "\n");
+
+        }
+
+
+        fclose(fichero);
+        g_free(filename);
+    }
+    gtk_widget_destroy(dialog);
+    gtk_widget_set_sensitive(save, FALSE);
 }
 
 void createFinalTableData(int timeLimit, FinalTable finalData[timeLimit + 1]) {
